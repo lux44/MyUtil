ff4d87f17f19866e2cd24acc085edeb0c0cd0750
shj <couquedas14@gmail.com>
2023-11-27 10:01:43 +0900

    Apply Git Ignore

--- app/src/main/java/com/example/myutil/utils/ConstVariables.kt
+++ app/src/main/java/com/example/myutil/utils/ConstVariables.kt
@@ -5,4 +5,5 @@ object ConstVariables {
     const val DEV_BASE_URL = "https://fauth-dev.fantoo.co.kr"//"https://fauth-dev.fantoo.co.kr" //"https://fauth.fantoo.co.kr"
     const val BASE_API_URL = "https://fapi.fantoo.co.kr"//"https://fapi-dev.fantoo.co.kr" //"https://fapi.fantoo.co.kr"
     const val DEV_BASE_API_URL = "https://fapi-dev.fantoo.co.kr"//"https://fapi-dev.fantoo.co.kr" //"https://fapi.fantoo.co.kr"
+
 }
