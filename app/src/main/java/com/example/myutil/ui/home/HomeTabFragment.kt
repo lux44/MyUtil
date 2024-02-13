7e9288dab1464a02df22126080468917a1ee87a9
lux44 <couquedas14@gmail.com>
2024-02-13 17:18:12 +0900

    - HomeTabViewModel file 생성 - adapter 변수 생성 - repository 들 만들어야 함

--- app/src/main/java/com/example/myutil/ui/home/HomeTabFragment.kt
+++ app/src/main/java/com/example/myutil/ui/home/HomeTabFragment.kt
@@ -1,11 +1,22 @@
 package com.example.myutil.ui.home
 
+import androidx.fragment.app.viewModels
 import com.example.myutil.databinding.FragmentHomeTabBinding
 import com.example.myutil.ui.common.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeTabFragment: BaseFragment<FragmentHomeTabBinding>(FragmentHomeTabBinding::inflate) {
+
+    // category list
+    private val categoryListAdapter = CategoryListAdapter()
+    private val categoryItems = listOf<Int>()
+
+    // view pager
+    lateinit var homeTabViewPagerAdapter: HomeTabPagerViewAdapter
+
+    private val homeTabViewModel: HomeTabViewModel by viewModels()
+
     override fun initUi() {
         TODO("Not yet implemented")
     }
