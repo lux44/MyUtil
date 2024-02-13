8c81c1e3308e67668d224ac116aa19b3236fa043
lux44 <couquedas14@gmail.com>
2024-02-13 14:04:11 +0900

    - HomeTabFragment xml finish - 각종 drawable file 추가

--- app/src/main/java/com/example/myutil/ui/home/HomeTabFragment.kt
+++ app/src/main/java/com/example/myutil/ui/home/HomeTabFragment.kt
@@ -1,7 +1,17 @@
 package com.example.myutil.ui.home
 
+import com.example.myutil.databinding.FragmentHomeTabBinding
+import com.example.myutil.ui.common.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeTabFragment {
+class HomeTabFragment: BaseFragment<FragmentHomeTabBinding>(FragmentHomeTabBinding::inflate) {
+    override fun initUi() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initUiActionEvent() {
+        TODO("Not yet implemented")
+    }
+
 }
