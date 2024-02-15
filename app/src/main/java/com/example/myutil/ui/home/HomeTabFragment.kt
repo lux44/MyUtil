8c36b9fc7512feb0d72e81b9230d0baf50df0fb5
lux44 <couquedas14@gmail.com>
2024-02-15 17:48:02 +0900

    - viewModel 기보 설정 - statusBar 색상 변경

--- app/src/main/java/com/example/myutil/ui/home/HomeTabFragment.kt
+++ app/src/main/java/com/example/myutil/ui/home/HomeTabFragment.kt
@@ -1,8 +1,11 @@
 package com.example.myutil.ui.home
 
+import android.os.Bundle
 import androidx.fragment.app.viewModels
+import com.example.myutil.R
 import com.example.myutil.databinding.FragmentHomeTabBinding
 import com.example.myutil.ui.common.BaseFragment
+import com.example.myutil.utils.setDarkStatusBarIcon
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -17,12 +20,25 @@ class HomeTabFragment: BaseFragment<FragmentHomeTabBinding>(FragmentHomeTabBindi
 
     private val homeTabViewModel: HomeTabViewModel by viewModels()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
     override fun initUi() {
-        TODO("Not yet implemented")
+        // 상태바 색상 변경
+        activity?.let {
+            activity?.window?.statusBarColor = it.getColor(R.color.bg_bg_light_gray_50)
+            it.setDarkStatusBarIcon()
+        }
     }
 
     override fun initUiActionEvent() {
         TODO("Not yet implemented")
+    }
+
+    private fun initToolbar() {
+
     }
 
 }
--- app/src/main/java/com/example/myutil/ui/home/HomeTabViewModel.kt
+++ app/src/main/java/com/example/myutil/ui/home/HomeTabViewModel.kt
@@ -1,12 +1,43 @@
 package com.example.myutil.ui.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myutil.repositories.DataStoreKey
+import com.example.myutil.repositories.DataStoreRepository
+import com.example.myutil.repositories.UserInfoRepository
+import com.example.myutil.utils.ModeChanger
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeTabViewModel @Inject constructor(
-
+    private val userInfoRepository: UserInfoRepository,
+    private val dataStoreRepository: DataStoreRepository
 ): ViewModel() {
+
+    var appMode: Int = ModeChanger.MODE_LIVE
+
+    var uId :String? = null
+    var accessToken :String? = null
+
+    private var isUser = false
+
+    init {
+        viewModelScope.launch {
+            dataStoreRepository.getBoolean(DataStoreKey.PREF_KEY_IS_LOGINED)?.let {
+                isUser = it
+                if (it) {
+                    accessToken = dataStoreRepository.getString(DataStoreKey.PREF_KEY_ACCESS_TOKEN)
+                    uId = dataStoreRepository.getString(DataStoreKey.PREF_KEY_UID)
+                }
+            }
+
+            appMode = ModeChanger.getInstance(dataStoreRepository).getMode()
+        }
+    }
+
+    fun isUser() = isUser
+
 
 }
