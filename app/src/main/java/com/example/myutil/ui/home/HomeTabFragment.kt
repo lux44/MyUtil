88d5a7ad4b28afe6c26dbd6f3b62f74cc472c471
lux44 <couquedas14@gmail.com>
2024-02-19 11:02:38 +0900

    - HomeTabFragment 에 showLogin 함수 추가

--- app/src/main/java/com/example/myutil/ui/home/HomeTabFragment.kt
+++ app/src/main/java/com/example/myutil/ui/home/HomeTabFragment.kt
@@ -7,9 +7,15 @@ import com.example.myutil.BuildConfig
 import com.example.myutil.R
 import com.example.myutil.databinding.FragmentHomeTabBinding
 import com.example.myutil.ui.common.BaseFragment
+import com.example.myutil.ui.menu.DialogButton
+import com.example.myutil.ui.menu.DialogClickType
+import com.example.myutil.ui.menu.DialogMessage
+import com.example.myutil.ui.menu.DialogTitle
+import com.example.myutil.ui.menu.MenuDialogFragment
 import com.example.myutil.utils.ModeChanger
 import com.example.myutil.utils.setDarkStatusBarIcon
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class HomeTabFragment: BaseFragment<FragmentHomeTabBinding>(FragmentHomeTabBinding::inflate) {
@@ -23,6 +29,8 @@ class HomeTabFragment: BaseFragment<FragmentHomeTabBinding>(FragmentHomeTabBindi
 
     private val homeTabViewModel: HomeTabViewModel by viewModels()
 
+    private lateinit var dialogFragment: MenuDialogFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -34,12 +42,14 @@ class HomeTabFragment: BaseFragment<FragmentHomeTabBinding>(FragmentHomeTabBindi
             activity?.window?.statusBarColor = it.getColor(R.color.bg_bg_light_gray_50)
             it.setDarkStatusBarIcon()
         }
+        initToolbar()
     }
 
     override fun initUiActionEvent() {
         TODO("Not yet implemented")
     }
 
+    // Tool bar 정의
     private fun initToolbar() {
         if (homeTabViewModel.appMode == ModeChanger.MODE_DEBUG && BuildConfig.DEBUG) {
             binding.tvDevText.visibility = View.VISIBLE
@@ -63,6 +73,44 @@ class HomeTabFragment: BaseFragment<FragmentHomeTabBinding>(FragmentHomeTabBindi
 
             }
         }
+    }
+
+    private fun showLoginMessage() {
+        val message = DialogMessage(
+            DialogTitle(
+                getString(R.string.r_need_login),
+                getString(R.string.se_r_need_login),
+                null
+            ),
+            DialogButton(getString(R.string.h_confirm), getString(R.string.c_cancel), true),
+            false
+        )
+
+        dialogFragment = MenuDialogFragment(message) { clickType ->
+            when (clickType) {
+                DialogClickType.OK -> {
+                    Timber.d("ok")
+                }
+                DialogClickType.CANCEL -> {
+                    Timber.d("cancel")
+                }
+            }
+            dismissDialog()
+        }
+        openDialog()
+    }
+
+    private fun dismissDialog() {
+        Timber.d("dismiss dialog")
+        dialogFragment.dismiss()
+    }
+
+    private fun openDialog() {
+        Timber.d("open dialog")
+        dialogFragment.show(
+            requireActivity().supportFragmentManager,
+            MenuDialogFragment.DIALOG_MENU
+        )
     }
 
 }
